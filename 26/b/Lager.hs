09b7d62d29b78081e590cb878bc40017d059afe5
Pikaru <45133988+GeneralPikaru@users.noreply.github.com>
2019-01-16 18:44:34 +0100

    Create Lager.hs

---
+++ 26/b/Lager.hs
@@ -0,0 +1,14 @@
+module Lager where
+import Data
+
+leeresLager :: Lager
+
+
+einlagern :: Int -> String -> Int -> Lager -> Lager
+
+
+suche :: Int -> Lager -> (String, Int)
+
+
+
+sucheNachGewicht :: Int -> Int -> Lager -> Lager
